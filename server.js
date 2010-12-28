5e27441784a9f844a19d26b82ba48afd9024e406
isaacs <i@izs.me>
2010-12-27 20:06:06 -0800

    Listen on PORT env if supplied

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var npm = require("npm")
   , server = require("./express-app")
+  , port = npm.config.get("npm_docsite__port") || process.env.PORT || 8008
 
 npm.load({}, function (er) {
   if (er) throw er
-  server.listen(npm.config.get("npm_docsite__port") || 8008)
+  server.listen(port)
 })
